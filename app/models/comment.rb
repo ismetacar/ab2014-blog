3dc9f135ce06b1b801d036f6a105a9c31fb7c175
ismetacar <acar.dogan@bil.omu.edu.tr>
2014-02-02 17:12:09 +0200

    blog baslangici Mersin

---
+++ app/models/comment.rb
@@ -0,0 +1,3 @@
+class Comment < ActiveRecord::Base
+  belongs_to :post         #comment, posta aittir. 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '15f0bfe45e145bd3a0e7bbd333aec5bde919442ce50ca85c40ecb3cb0e08435d1826949de4b15c4faa618398f7e6887b7ab046852c9a95e45f8cb5bfe3053283'
